1abef1d895ab47f31856f476ec9e674b95ef770b
Filip-XoXo <x.malasorte.x@gmail.com>
2021-05-15 19:29:44 +0200

    komit

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ from resources.store import Store,StoreList
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
-
+#uri = os.getenv("DATABASE_URL")
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 
 # DATABASE_URL predstavuje premennu(variable), ktoru pre nas vytvoril Heroku
