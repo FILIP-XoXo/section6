0fcd4e8b5bd9f5cf11d1dcfe27b5a77f603e2127
Filip-XoXo <x.malasorte.x@gmail.com>
2021-05-17 23:44:13 +0200

    api nove

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ api = Api(app)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.Text)
+    public_id = db.Column(db.String(110), unique=True)
+    name = db.Column(db.String(60))
+    password = db.Column(db.String(100))
     admin = db.Column(db.Boolean)
 
 class Todo(db.Model):
